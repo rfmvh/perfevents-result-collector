01c621e768d8fb829595c9a71d9c9dd823fb3597
Jan Šmerda <jansmerda@dhcp-2-138.brq.redhat.com>
2016-05-24 10:11:21 +0200

    Added show-virt.py

---
+++ scripts/show-virt.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+import re
+import os
+import sys
+
+from dbinterface import *
+from optparse import OptionParser
+
+optparser = OptionParser()
+optparser.set_defaults(listmode=0)
+optparser.add_option("", "--name", action="store", dest="name")
+optparser.add_option("", "--csv", action="store_true", default=False, dest="csv")
+
+(options, args) = optparser.parse_args() 
+
+# open DB
+db = DBConnection()
+
+
+def show_virt(name, csv):
+  if name:
+    sql_query = 'SELECT * FROM virt WHERE name = %(virt_name)s;'
+    sql_params = {'virt_name': name}
+    
+    results = db.select(sql_query, sql_params)
+    
+    if not results:
+      print "Error: Name does not exist"
+      sys.exit(1)     
+                
+  else:
+    sql_query = 'SELECT * FROM virt;'
+    
+    results = db.select(sql_query)
+      
+  if csv:
+    print "#name"
+    for line in results:
+      print "%s" % (line[1])
+    
+  else:
+    print "name"
+    for line in results:
+      print "%s" % (line[1])
+
+show_virt(options.name, options.csv)
