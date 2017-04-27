949badd0b9de78c05b434a8de581edade41a91e1
djasek <davijasek@gmail.com>
2017-04-27 17:56:44 +0200

    delete line

--- scripts/dbinterface.py
+++ scripts/dbinterface.py
@@ -28,7 +28,6 @@ class DBConnection(object):
     @param use_localhost: set to True if you intent to connect to db running on localhost
     """
 
-
     self.dryrun = dryrun
     self.debug = debug
 
