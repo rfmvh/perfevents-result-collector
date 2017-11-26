46726958ebbdf8d9aa4443ae63f46154e67bbf68
Martin Necas <necas.marty@gmail.com>
2017-11-26 18:35:54 +0100

    clean up add-experiment

--- perfresultcollector/add-experiment.py
+++ perfresultcollector/add-experiment.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python
 
-import sys
-import re
-import os
+import argparse
 
 from dbinterface import *
-
-import argparse
 
 parser = argparse.ArgumentParser()
 parser.set_defaults(listmode=0)
@@ -22,34 +18,30 @@ options = parser.parse_args()
 db = DBConnection()
 
 
-def add(name, cmd, desc, systWide, force):
+def add(name, cmd, desc, syst_wide, force):
     if not name:
         print "Error: Insert the name of the experiment"
         sys.exit(1)
 
+    sql_query = 'SELECT exp_id FROM experiments WHERE name = %(exp_name)s;'
+    sql_params = {'exp_name': name}
+    results = db.query(sql_query, sql_params)
+
+    if not results:
+        sql_query_insert = 'INSERT INTO experiments (name, cmd, description, systemwide)' \
+                           ' VALUES (%(exp_name)s, %(exp_cmd)s, %(exp_desc)s, %(exp_systemwide)s);'
+        sql_params_insert = {'exp_name': name, 'exp_cmd': cmd, 'exp_desc': desc, 'exp_systemwide': syst_wide}
+        db.query(sql_query_insert, sql_params_insert)
+
     else:
-        sql_query = 'SELECT exp_id FROM experiments WHERE name = %(exp_name)s;'
-        sql_params = {'exp_name': name}
-
-        results = db.query(sql_query, sql_params)
-
-        if not results:
-            # Vlozeni do db
-            sql_query_insert = 'INSERT INTO experiments (name, cmd, description, systemwide) VALUES (%(exp_name)s, %(exp_cmd)s, %(exp_desc)s, %(exp_systemwide)s);'
-            sql_params_insert = {'exp_name': name, 'exp_cmd': cmd, 'exp_desc': desc, 'exp_systemwide': systWide}
-            db.query(sql_query_insert, sql_params_insert)
-
-        else:
-            if not force:
-                # Error
-                print "Error: Name exists"
-                sys.exit(1)
-
-            else:
-                # Prepsat v DB - update
-                sql_query_update = 'UPDATE experiments SET cmd = %(exp_cmd)s, description = %(exp_desc)s, systemwide = %(exp_systemwide)s WHERE name=%(exp_name)s;'
-                sql_params_update = {'exp_cmd': cmd, 'exp_desc': desc, 'exp_systemwide': systWide, 'exp_name': name}
-            db.query(sql_query_update, sql_params_update)
+        if not force:
+            print "Error: Name exists"
+            sys.exit(1)
+        sql_query_update = 'UPDATE experiments SET cmd = %(exp_cmd)s, description = %(exp_desc)s,' \
+                           ' systemwide = %(exp_systemwide)s WHERE name=%(exp_name)s;'
+        sql_params_update = {'exp_cmd': cmd, 'exp_desc': desc, 'exp_systemwide': syst_wide, 'exp_name': name}
+        db.query(sql_query_update, sql_params_update)
 
 
-add(options.name, options.cmd, options.description, options.systemwide, options.force)
+if __name__ == '__main__':
+    add(options.name, options.cmd, options.description, options.systemwide, options.force)
