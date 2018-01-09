47e394f5b7a63fa290a1b25fb3acd8f9ef1dd96c
Martin Necas <necas.marty@gmail.com>
2018-01-09 12:16:35 +0100

    correct setting logging level

--- perfresultcollector/logger.py
+++ perfresultcollector/logger.py
@@ -9,28 +9,39 @@ class Logger(object):
         self.name = name_of_file
         self.config = ConfigParser.ConfigParser()
         self.config.read(self.path_to_conf_file)
+        self.refresh_logger()
+
+    def refresh_logger(self):
         log_level = self.config.get("Defaults", "logging_level")
         logging.basicConfig(filename='../logger_out.log',
-                            level=getattr(logging, str(log_level)))
-        self.LOGGER = logging.getLogger(name_of_file)
+                            level=getattr(logging, str(log_level.upper())))
+        self.LOGGER = logging.getLogger(self.name)
 
     def set_logger_level(self, level=""):
-        self.config.read(self.path_to_log_file)
-        self.config.set("logging_level", level)
+        self.config.read(self.path_to_conf_file)
+        self.config.set("Defaults", "logging_level", level.upper())
+        with open(self.path_to_conf_file, 'wb') as configfile:
+            self.config.write(configfile)
 
     def get_logger_level(self):
         self.config.read(self.path_to_conf_file)
         return self.config.get("Defaults", "logging_level")
 
     def debug(self, text):
+        self.refresh_logger()
         self.LOGGER.debug(text)
 
     def info(self, text):
+        self.refresh_logger()
         self.LOGGER.info(text)
 
     def warning(self, text):
+        self.refresh_logger()
         self.LOGGER.warning(text)
 
 
 loger = Logger(__name__)
 loger.debug("texit")
+loger.set_logger_level("info")
+loger.debug("asdasd")
+loger.warning("random")
